af127d6d90eeed0ac5fe7cf4afa83049b5952092
Atiq <cross.016x@gmail.com>
2023-09-30 22:16:39 +0600

    Beginner 01 done

--- Basic For Begginers/excercise_01.py
+++ Basic For Begginers/excercise_01.py
@@ -1,6 +1,7 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
+# Given two integer numbers, return their product only if the product is
+# equal to or lower than 1000. Otherwise, return their sum.
 
-# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 # Given 1:
 #   number1 = 20
 #   number2 = 30
@@ -14,3 +15,15 @@
 
 # Expected Output:
 #   The result is 70
+
+
+def multiplication_or_sum(num1, num2):
+    product = num1 * num2
+    if product <= 1000:
+        return product
+    else:
+        return num1 + num2
+
+
+result = multiplication_or_sum(10, 120)
+print("The result is :", result)
